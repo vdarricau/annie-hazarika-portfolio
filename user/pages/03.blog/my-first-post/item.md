7c4ae9e1c30752fca12585b7e1ec2c088af91d5e
Valentin Darricau <darricau.valentin@gmail.com>
2023-03-06 13:55:27 +0700

    Create new theme with default page

--- user/pages/03.blog/my-first-post/item.md
+++
@@ -1,6 +0,0 @@
----
-title: 'My first post'
-media_order: alien3655.webp
----
-
-Sup broooo
---
+++ user/themes/custom/custom.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Custom extends Theme
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [];
+    }
+}
